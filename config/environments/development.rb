8f96060ea633e27ed25e62be7302823c13b9773d
Igor <igor.warlog@gmail.com>
2022-05-12 22:51:45 -0300

    Gems configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+# Configuração do mailcatche
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+config.action_mailer.raise_delivery_errors = false
+
+#Configuração do Devise
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
